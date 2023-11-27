2738449fee56a30fd7df7fe6ab235f9e5ecabf21
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-11-26 22:27:34 -0300

    Update exercicio2_lista3.c
    atualização

--- exercicio2_lista3.c
+++ exercicio2_lista3.c
@@ -9,8 +9,8 @@ int main() {
 
     scanf("%d %lf %lf", &meses, &aporte, &taxa);
 
-    for (int i = 1; i <= meses; i++) {
-        montante += aporte * (1 + taxa);
+   for (int i = 1; i <= meses; i++) {
+        montante = (aporte * (1 + taxa)) *((pow(1 + taxa, i) - 1) / taxa);
         printf("Montante ao fim do mes %d: R$ %.2lf\n", i, montante);
     }
 
