f91a1b18c2fa05b26401125771772cd864dfe288
Estevão Lins Maia <114420545+ttstive@users.noreply.github.com>
2023-11-27 21:43:44 -0300

    Update exercicio3_lista3.c
    correto

--- exercicio3_lista3.c
+++ exercicio3_lista3.c
@@ -1,44 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-char* strlwr(char* str) {
-    unsigned char *p = (unsigned char *)str;
 
-    while (*p) {
-        *p = tolower((unsigned char)*p);
-        p++;
-    }
-
-    return str;
-}
-
-int validarPlaca(const char *placa) {
+int validarPlaca(char *placa) {
+    int formatoValido = 0;
     int comprimento = strlen(placa);
 
-    if ((comprimento == 8 && placa[3] == '-' && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2])) ||
-        (comprimento == 7 && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) &&
-        isdigit(placa[3]) && isalpha(placa[4]) && isdigit(placa[5]) && isdigit(placa[6]))) {
-        return 1;
+    if (comprimento == 8 && placa[3] == '-' && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2])) {
+        formatoValido = 1;
+    } else if (comprimento == 7 && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) &&
+               isdigit(placa[3]) && isalpha(placa[4]) && isdigit(placa[5]) && isdigit(placa[6])) {
+        formatoValido = 1;
     }
 
-    return 0;
+    return formatoValido;
 }
 
-int validarDia(const char *dia) {
-    const char *diasValidos[] = {"SEGUNDA-FEIRA", "TERCA-FEIRA", "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SABADO", "DOMINGO"};
-    int numDias = sizeof(diasValidos) / sizeof(diasValidos[0]);
+int validarDia(char *dia) {
+    char diasValidos[7][15] = {"SEGUNDA-FEIRA", "TERCA-FEIRA", "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SABADO", "DOMINGO"};
+    int diaValido = 0;
 
-    for (int i = 0; i < numDias; i++) {
+    for (int i = 0; i < 7; i++) {
         if (strcmp(dia, diasValidos[i]) == 0) {
-            return 1;
+            diaValido = 1;
+            break;
         }
     }
 
-    return 0;
+    return diaValido;
 }
 
-// Função imprimirResultado atualizada
-int imprimirResultado(const char *placa, const char *dia, int placaValida, int diaValido, int podeCircular) {
+int imprimirResultado(char *placa, char *dia, int placaValida, int diaValido, int podeCircular) {
+    char diaLower[15];
+    strcpy(diaLower, dia);
+    strlwr(diaLower);
+
     if (!placaValida) {
         printf("Placa invalida\n");
     }
@@ -50,25 +46,17 @@ int imprimirResultado(const char *placa, const char *dia, int placaValida, int d
     if (podeCircular && (strcmp(dia, "SABADO") == 0 || strcmp(dia, "DOMINGO") == 0)) {
         printf("Nao ha proibicao no fim de semana\n");
     } else if (podeCircular) {
-        printf("%s pode circular %s\n", placa, dia);
+        printf("%s pode circular %s\n", placa, diaLower);
     } else if (placaValida && diaValido) {
-        printf("%s nao pode circular %s\n", placa, dia);
+        printf("%s nao pode circular %s\n", placa, diaLower);
     }
 
     return 0;
 }
 
-
 int verificarRodizio(const char *placa, const char *dia) {
     int terminacao = placa[strlen(placa) - 1] - '0';
-    char diaUpper[15];
-    strcpy(diaUpper, dia);
 
-    for (int i = 0; diaUpper[i]; i++) {
-        diaUpper[i] = tolower(dia[i]);
-    }
-
-    // Usando switch case para verificar o rodízio
     switch (terminacao) {
         case 0:
         case 1:
@@ -114,14 +102,6 @@ int main() {
     scanf("%s", placa);
     scanf("%s", dia);
 
-    // Convertendo para maiúsculas
-    for (int i = 0; placa[i]; i++) {
-        placa[i] = toupper(placa[i]);
-    }
-    for (int i = 0; dia[i]; i++) {
-        dia[i] = toupper(dia[i]);
-    }
-
     int placaValida = validarPlaca(placa);
     int diaValido = validarDia(dia);
 
@@ -131,8 +111,7 @@ int main() {
     }
 
     int podeCircular = !verificarRodizio(placa, dia);
-    // Alterando a saída para que o dia da semana esteja em minúsculo
-    imprimirResultado(placa, strlwr(dia), placaValida, diaValido, podeCircular);
+    imprimirResultado(placa, dia, placaValida, diaValido, podeCircular);
 
     return 0;
 }
